1187283cc7fca37c3855894ad33ed37dfc47d0da
zex <the_zex@yahoo.com>
2021-10-03 11:26:21 +0100

    first commit

--- May2021A2a/main.c
+++ May2021A2a/main.c
@@ -8,8 +8,53 @@
 
 #include <stdio.h>
 
+#include <string.h>
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    //This array would hold the string upto 150 char
+    char string_array[150];
+    // these values hold the counts
+    int digit_count = 0;
+    int letter_count = 0;
+    int other_count = 0;
+    //Output to the user
+    printf("Enter any string:");
+    //recieve input
+    scanf("%s", &string_array);
+    //check the length
+    int length = strlen(string_array);
+    if (length < 10) {
+        printf("You must have at least 10 characters.");
+        return 0;
+    }
+    //iterate over the string
+    for (int i =0; i < length; i++) {
+        char ch = string_array[i];
+        if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+        {
+            letter_count++;
+        }
+        else if(ch >= '0' && ch <= '9')
+        {
+            digit_count++;
+        }
+        else
+        {
+            other_count++;
+        }
+    }
+    if (other_count > 0) {
+        printf("You can only have letters and digits.");
+    }
+    else if(letter_count < 2) {
+        printf("You must have at least 2 letters.");
+    }
+    else if(digit_count < 2) {
+        printf("You must have at least 2 digits.");
+    }
+    else {
+        printf("You have entered a valid password.");
+    }
+
     return 0;
 }
